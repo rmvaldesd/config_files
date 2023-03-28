d1294476614b8d425a2818a312ca4437d0434889
Rodrigo Valdes <rmvaldesd@gmail.com>
2023-03-27 22:51:56 -0300

    updating neovim dap and tmux config

--- dotconfig/nvim/lua/plugins/dap.lua
+++ dotconfig/nvim/lua/plugins/dap.lua
@@ -1,27 +1,51 @@
-
 require("dap").adapters.lldb = {
-	type = "executable",
-	command = "/usr/bin/lldb-vscode", -- adjust as needed
-	name = "lldb",
+  type = "executable",
+  command = "/usr/bin/lldb-vscode", -- adjust as needed
+  name = "lldb",
 }
 
 local lldb = {
-	name = "Launch lldb",
-	type = "lldb", -- matches the adapter
-	request = "launch", -- could also attach to a currently running process
-	program = function()
-		return vim.fn.input(
-			"Path to executable: ",
-			vim.fn.getcwd() .. "/",
-			"file"
-		)
-	end,
-	cwd = "${workspaceFolder}",
-	stopOnEntry = false,
-	args = {},
-	runInTerminal = false,
+  name = "Launch lldb",
+  type = "lldb", -- matches the adapter
+  request = "launch", -- could also attach to a currently running process
+  program = function()
+    return vim.fn.input(
+      "Path to executable: ",
+      vim.fn.getcwd() .. "/",
+      "file"
+    )
+  end,
+  cwd = "${workspaceFolder}",
+  stopOnEntry = false,
+  args = {},
+  runInTerminal = false,
 }
 
 require('dap').configurations.rust = {
-	lldb -- different debuggers or more configurations can be used here
+  lldb -- different debuggers or more configurations can be used here
+}
+
+-- https://github.com/go-delve/delve/blob/master/Documentation/usage/dlv_dap.md
+require("dap").configurations.go = {
+  {
+    type = "delve",
+    name = "Debug",
+    request = "launch",
+    program = "${file}"
+  },
+  {
+    type = "delve",
+    name = "Debug test", -- configuration for debugging test files
+    request = "launch",
+    mode = "test",
+    program = "${file}"
+  },
+  -- works with go.mod packages and sub packages
+  {
+    type = "delve",
+    name = "Debug test (go.mod)",
+    request = "launch",
+    mode = "test",
+    program = "./${relativeFileDirname}"
+  }
 }
--- dotconfig/nvim/lua/plugins/lsp-settings.lua
+++ dotconfig/nvim/lua/plugins/lsp-settings.lua
@@ -1,26 +1,27 @@
 -- ----------------------nvim-lsp-installer setup-----------------------
 local util = require 'lspconfig.util'
 require("mason").setup({
-    -- List of servers to automatically install
-    -- automatically detect which servers to install (based on which servers are set up via lspconfig)
-    ui = {
-        icons = {
-            server_installed = "✓",
-            server_pending = "➜",
-            server_uninstalled = "✗"
-        }
+  -- List of servers to automatically install
+  -- automatically detect which servers to install (based on which servers are set up via lspconfig)
+  ui = {
+    icons = {
+      server_installed = "✓",
+      server_pending = "➜",
+      server_uninstalled = "✗"
     }
+  }
 })
 
-require("mason-lspconfig").setup{
-    ensure_installed = { 'pyright', 'gopls', 'eslint', 'tsserver', 'bashls', 'cssls', 'html', 'lua_ls', 'jsonls', 'clangd', 'lemminx' },
+require("mason-lspconfig").setup {
+  ensure_installed = { 'pyright', 'gopls', 'eslint', 'tsserver', 'bashls', 'cssls', 'html', 'lua_ls', 'jsonls',
+    'clangd', 'lemminx' },
 }
 
 
 -- ------------------------- LSP Setup ---------------------------------
 -- Mappings.
 -- See `:help vim.diagnostic.*` for documentation on any of the below functions
-local opts = { noremap=true, silent=true }
+local opts = { noremap = true, silent = true }
 vim.keymap.set('n', '<space>e', vim.diagnostic.open_float, opts)
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
@@ -36,7 +37,7 @@ local on_attach = function(client, bufnr)
 
   -- Mappings.
   -- See `:help vim.lsp.*` for documentation on any of the below functions
-  local bufopts = { noremap=true, silent=true, buffer=bufnr }
+  local bufopts = { noremap = true, silent = true, buffer = bufnr }
   vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
   vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
   vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
@@ -60,48 +61,48 @@ end
 local cmp = require 'cmp'
 local luasnip = require('luasnip')
 cmp.setup {
-    snippet = {
-        expand = function(args)
-            luasnip.lsp_expand(args.body)
-        end,
+  snippet = {
+    expand = function(args)
+      luasnip.lsp_expand(args.body)
+    end,
+  },
+  window = {
+    -- completion = cmp.config.window.bordered(),
+    -- documentation = cmp.config.window.bordered(),
+  },
+  mapping = cmp.mapping.preset.insert({
+    ['<C-d>'] = cmp.mapping.scroll_docs(-4),
+    ['<C-f>'] = cmp.mapping.scroll_docs(4),
+    ['<C-Space>'] = cmp.mapping.complete(),
+    ['<CR>'] = cmp.mapping.confirm {
+      behavior = cmp.ConfirmBehavior.Replace,
+      select = true,
     },
-    window = {
-      -- completion = cmp.config.window.bordered(),
-      -- documentation = cmp.config.window.bordered(),
-    },
-    mapping = cmp.mapping.preset.insert({
-        ['<C-d>'] = cmp.mapping.scroll_docs(-4),
-        ['<C-f>'] = cmp.mapping.scroll_docs(4),
-        ['<C-Space>'] = cmp.mapping.complete(),
-        ['<CR>'] = cmp.mapping.confirm {
-            behavior = cmp.ConfirmBehavior.Replace,
-            select = true,
-        },
-        ['<Tab>'] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-                cmp.select_next_item()
-            elseif luasnip.expand_or_jumpable() then
-                luasnip.expand_or_jump()
-            else
-                fallback()
-            end
-        end, { 'i', 's' }),
-        ['<S-Tab>'] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-                cmp.select_prev_item()
-            elseif luasnip.jumpable(-1) then
-                luasnip.jump(-1)
-            else
-                fallback()
-            end
-        end, { 'i', 's' }),
-    }),
-    sources = {
-        { name = 'nvim_lsp'},
-        { name = 'luasnip' },
-        { name = 'buffer' },
-        { name = 'nvim_lsp_signature_help' }
-    },
+    ['<Tab>'] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_next_item()
+      elseif luasnip.expand_or_jumpable() then
+        luasnip.expand_or_jump()
+      else
+        fallback()
+      end
+    end, { 'i', 's' }),
+    ['<S-Tab>'] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_prev_item()
+      elseif luasnip.jumpable(-1) then
+        luasnip.jump(-1)
+      else
+        fallback()
+      end
+    end, { 'i', 's' }),
+  }),
+  sources = {
+    { name = 'nvim_lsp' },
+    { name = 'luasnip' },
+    { name = 'buffer' },
+    { name = 'nvim_lsp_signature_help' }
+  },
 }
 
 -- -----------------------end nvim-cmp configuration ---------------------
@@ -119,24 +120,24 @@ local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
 
 -- LSPs with default setup: bashls (Bash), cssls (CSS), html (HTML), clangd (C/C++), jsonls (JSON)
-for _, lsp in ipairs { 'tsserver', 'bashls', 'cssls', 'html', 'clangd', 'jsonls'} do
-      require('lspconfig')[lsp].setup {
-        on_attach = on_attach,
-        flags=lsp_flags,
-        capabilities = capabilities,
-      }
-end
-
--- LSPs with no default setup
-require('lspconfig')['gopls'].setup{
+for _, lsp in ipairs { 'tsserver', 'bashls', 'cssls', 'html', 'clangd', 'jsonls', 'lua_ls' } do
+  require('lspconfig')[lsp].setup {
     on_attach = on_attach,
     flags = lsp_flags,
     capabilities = capabilities,
-    filetypes = { 'go', 'gomod', 'gowork', 'gotmpl' },
-    root_dir = function(fname)
-      return util.root_pattern 'go.work'(fname) or util.root_pattern('go.mod', '.git')(fname)
-    end,
-    single_file_support = true,
+  }
+end
+
+-- LSPs with no default setup
+require('lspconfig')['gopls'].setup {
+  on_attach = on_attach,
+  flags = lsp_flags,
+  capabilities = capabilities,
+  filetypes = { 'go', 'gomod', 'gowork', 'gotmpl' },
+  root_dir = function(fname)
+    return util.root_pattern 'go.work' (fname) or util.root_pattern('go.mod', '.git')(fname)
+  end,
+  single_file_support = true,
 }
 
 
@@ -148,55 +149,54 @@ local python_root_files = {
   'Pipfile',
   'pyrightconfig.json',
 }
-require('lspconfig')['pyright'].setup{
-    on_attach = on_attach,
-    flags = lsp_flags,
-    capabilities = capabilities,
-    filetypes = { 'python' },
-    root_dir = util.root_pattern(unpack(python_root_files)),
-    single_file_support = true,
-    settings = {
-      python = {
-        analysis = {
-          autoSearchPaths = true,
-          useLibraryCodeForTypes = true,
-          diagnosticMode = 'workspace',
-        },
+require('lspconfig')['pyright'].setup {
+  on_attach = on_attach,
+  flags = lsp_flags,
+  capabilities = capabilities,
+  filetypes = { 'python' },
+  root_dir = util.root_pattern(unpack(python_root_files)),
+  single_file_support = true,
+  settings = {
+    python = {
+      analysis = {
+        autoSearchPaths = true,
+        useLibraryCodeForTypes = true,
+        diagnosticMode = 'workspace',
       },
     },
+  },
 }
 
 
-require('lspconfig')['dartls'].setup{
-    on_attach = on_attach,
-    flags = lsp_flags,
-    capabilities = capabilities,
-    filetypes = { 'dart' },
-    root_dir = util.root_pattern 'pubspec.yaml',
-    single_file_support = true,
-    init_options = {
-      onlyAnalyzeProjectsWithOpenFiles = true,
-      suggestFromUnimportedLibraries = true,
-      closingLabels = true,
-      outline = true,
-      flutterOutline = true,
+require('lspconfig')['dartls'].setup {
+  on_attach = on_attach,
+  flags = lsp_flags,
+  capabilities = capabilities,
+  filetypes = { 'dart' },
+  root_dir = util.root_pattern 'pubspec.yaml',
+  single_file_support = true,
+  init_options = {
+    onlyAnalyzeProjectsWithOpenFiles = true,
+    suggestFromUnimportedLibraries = true,
+    closingLabels = true,
+    outline = true,
+    flutterOutline = true,
+  },
+  settings = {
+    dart = {
+      completeFunctionCalls = true,
+      showTodos = true,
     },
-    settings = {
-      dart = {
-        completeFunctionCalls = true,
-        showTodos = true,
-      },
-    },
+  },
 }
 
 
-require('lspconfig')['rust_analyzer'].setup{
-    on_attach = on_attach,
-    flags = lsp_flags,
-    capabilities = capabilities,
-    -- Server-specific settings...
-    settings = {
-      ["rust-analyzer"] = {}
-    }
+require('lspconfig')['rust_analyzer'].setup {
+  on_attach = on_attach,
+  flags = lsp_flags,
+  capabilities = capabilities,
+  -- Server-specific settings...
+  settings = {
+    ["rust-analyzer"] = {}
+  }
 }
-
