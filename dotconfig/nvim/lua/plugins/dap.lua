c38661f91c009718d61f5ae6737a374f500e51ea
Rodrigo Valdes <rmvaldesd@gmail.com>
2023-04-03 09:48:47 -0400

    adding flutter debug support to dap.lua

--- dotconfig/nvim/lua/plugins/dap.lua
+++ dotconfig/nvim/lua/plugins/dap.lua
@@ -79,6 +79,26 @@ require("dap").configurations.go = {
   }
 }
 
+require("dap").adapters.dart = {
+  type = "executable",
+  -- As of this writing, this functionality is open for review in https://github.com/flutter/flutter/pull/91802
+  command = "flutter",
+  args = { "debug_adapter" }
+}
+require("dap").configurations.dart = {
+  {
+    type = "dart",
+    request = "launch",
+    name = "Launch Flutter Program",
+    -- The nvim-dap plugin populates this variable with the filename of the current buffer
+    program = "${file}",
+    -- The nvim-dap plugin populates this variable with the editor's current working directory
+    cwd = "${workspaceFolder}",
+    -- This gets forwarded to the Flutter CLI tool, substitute `linux` for whatever device you wish to launch
+    toolArgs = { "-d", "iphone 14" }
+  }
+}
+
 require("dap").listeners.after.event_initialized["dapui_config"] = function()
   require("dapui").open()
 end
