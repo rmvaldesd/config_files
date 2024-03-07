571629d19d655aa15a34c93b216259580d6d0385
Rodrigo Valdes <rodrigo@Rodrigos-MacBook-Pro.local>
2024-03-06 23:03:28 -0300

    configure lazy.nvim p1

--- dotconfig/nvim/lua/plugins.lua
+++ dotconfig/nvim/lua/plugins.lua
@@ -1,91 +1,87 @@
-local ensure_packer = function()
-  local fn = vim.fn
-  local install_path = fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
-  if fn.empty(fn.glob(install_path)) > 0 then
-    fn.system({ 'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path })
-    vim.cmd [[packadd packer.nvim]]
-    return true
-  end
-  return false
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
 end
+vim.opt.rtp:prepend(lazypath)
 
-local packer_bootstrap = ensure_packer()
+return require('lazy').setup({
+  'nvim-lua/plenary.nvim',
+  'nvim-telescope/telescope-symbols.nvim',
+  'wbthomason/packer.nvim',
+  'ojroques/vim-oscyank',
+  --'williamboman/nvim-lsp-installer'
+  'williamboman/mason.nvim',
+  'williamboman/mason-lspconfig.nvim',
 
-return require('packer').startup(function(use)
-  use { 'nvim-lua/plenary.nvim' }
-  use { 'nvim-telescope/telescope-symbols.nvim' }
-  use 'wbthomason/packer.nvim'
-  use 'ojroques/vim-oscyank'
-  --use 'williamboman/nvim-lsp-installer'
-  use { 'williamboman/mason.nvim' }
-  use { 'williamboman/mason-lspconfig.nvim' }
+  'neovim/nvim-lspconfig',
+  'tpope/vim-commentary',
+  'tpope/vim-fugitive',
+  'tpope/vim-surround',
 
-  use 'neovim/nvim-lspconfig'
-  use 'tpope/vim-commentary'
-  use 'tpope/vim-fugitive'
-  use 'tpope/vim-surround'
+  'ellisonleao/gruvbox.nvim',
+  'vim-airline/vim-airline',
+  'vim-airline/vim-airline-themes',
+  'norcalli/nvim-colorizer.lua',
 
-  use { 'ellisonleao/gruvbox.nvim' }
-  use 'vim-airline/vim-airline'
-  use 'vim-airline/vim-airline-themes'
-  use { 'norcalli/nvim-colorizer.lua' }
-
-  use 'rafcamlet/nvim-luapad'
-  use 'L3MON4D3/LuaSnip'
-  use 'saadparwaiz1/cmp_luasnip'
-  use 'hrsh7th/nvim-cmp'
-  use 'hrsh7th/cmp-nvim-lsp'
-  use 'hrsh7th/cmp-buffer'
-  use { 'numtostr/BufOnly.nvim', cmd = 'BufOnly' }
-  use {
+  'rafcamlet/nvim-luapad',
+  'L3MON4D3/LuaSnip',
+  'saadparwaiz1/cmp_luasnip',
+  'hrsh7th/nvim-cmp',
+  'hrsh7th/cmp-nvim-lsp',
+  'hrsh7th/cmp-buffer',
+  { 'numtostr/BufOnly.nvim',         cmd = 'BufOnly' },
+  {
     'akinsho/flutter-tools.nvim',
-    requires = 'nvim-lua/plenary.nvim',
+    dependencies = 'nvim-lua/plenary.nvim',
     config = function()
       require("plugins/flutter-tools").setup()
     end
-  }
+  },
 
   -- notifications
-  use { 'rcarriga/nvim-notify' }
+  'rcarriga/nvim-notify',
 
   -- Telescope
-  use { 'nvim-telescope/telescope.nvim', requires = 'nvim-lua/plenary.nvim' }
-  use { "nvim-telescope/telescope-file-browser.nvim" }
-  use { 'nvim-telescope/telescope-fzf-native.nvim', run = 'make' }
-  use 'kyazdani42/nvim-web-devicons'
+  { 'nvim-telescope/telescope.nvim', dependencies = 'nvim-lua/plenary.nvim' },
+  "nvim-telescope/telescope-file-browser.nvim",
+  { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
+  'kyazdani42/nvim-web-devicons',
 
   -- Notes - Wiki
-  use { 'lervag/wiki.vim' }
+  'lervag/wiki.vim',
 
   -- browse things.
-  use { 'preservim/tagbar' }
-  use { 'preservim/nerdtree' }
+  'preservim/tagbar',
+  'preservim/nerdtree',
 
   -- Debugging
-  use { "mfussenegger/nvim-dap" }
-  use { "mfussenegger/nvim-dap-python" }
-  use { "leoluz/nvim-dap-go" }
-  use { "rcarriga/nvim-dap-ui" }
+  'mfussenegger/nvim-dap',
+  'mfussenegger/nvim-dap-python',
+  'leoluz/nvim-dap-go',
+  'rcarriga/nvim-dap-ui',
 
   -- Formatting
-  use "lukas-reineke/lsp-format.nvim"
+  'lukas-reineke/lsp-format.nvim',
 
   -- Treesitter
-  use { 'nvim-treesitter/nvim-treesitter',
-    run = function()
+  {
+    'nvim-treesitter/nvim-treesitter',
+    build = function()
       local ts_update = require('nvim-treesitter.install').update({ with_sync = true })
       ts_update()
     end,
-  }
+  },
 
-  use {
+  {
     "ThePrimeagen/harpoon",
-    requires = 'nvim-lua/plenary.nvim'
-  }
+    dependencies = 'nvim-lua/plenary.nvim'
+  },
 
-  -- Automatically set up your configuration after cloning packer.nvim
-  -- Put this at the end after all plugins
-  if packer_bootstrap then
-    require('packer').sync()
-  end
-end)
+})
