8e5900b8152257e58d4e421278fdd6a67c2e3a7c
Rodrigo Valdés <rmvaldesd@gmail.com>
2025-05-26 11:22:11 -0400

    floaterm, copilot, avante

--- dotconfig/nvim/init.lua
+++ dotconfig/nvim/init.lua
@@ -2,14 +2,14 @@ vim.env.PATH = vim.env.NVIM_PATH or vim.env.PATH
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 vim.opt.nu = true
@@ -26,11 +26,12 @@ vim.opt.wrap = false
 vim.opt.ignorecase = true
 vim.g.mapleader = " "
 vim.o.termguicolors = true
-vim.g.netrw_banner = 0       -- gets rid of the annoying banner for netrw
+vim.g.netrw_banner = 0 -- gets rid of the annoying banner for netrw
 vim.g.netrw_browse_split = 4 -- open in prior window
-vim.g.netrw_altv = 1         -- change from left splitting to right splitting
-vim.g.netrw_liststyle = 3    -- tree style view in netrw
+vim.g.netrw_altv = 1 -- change from left splitting to right splitting
+vim.g.netrw_liststyle = 3 -- tree style view in netrw
+vim.opt.laststatus = 3
 
-require('lazy').setup('plugins')
+require("lazy").setup("plugins")
 require("plugin-settings")
 require("keymappings")
--- dotconfig/nvim/lua/plugins/init.lua
+++ dotconfig/nvim/lua/plugins/init.lua
@@ -1,149 +1,97 @@
 return {
-  {
-    "nvim-lua/plenary.nvim",
-    "nvim-telescope/telescope-symbols.nvim",
-    "wbthomason/packer.nvim",
-    "ojroques/vim-oscyank",
-    --'williamboman/nvim-lsp-installer'
-    {
-      "williamboman/mason.nvim",
-      config = function()
-        require("mason").setup({
-          ui = {
-            icons = {
-              server_installed = "✓",
-              server_pending = "➜",
-              server_uninstalled = "✗",
-            },
-          },
-        })
-      end,
-    },
-    {
-      "williamboman/mason-lspconfig.nvim",
-      config = function()
-        require("mason-lspconfig").setup({
-          ensure_installed = {
-            "pyright",
-            "gopls",
-            "eslint",
-            "bashls",
-            "html",
-            "lua_ls",
-            "jsonls",
-            "clangd",
-            "lemminx",
-          },
-        })
-      end,
-    },
+	{
+		"nvim-lua/plenary.nvim",
+		"nvim-telescope/telescope-symbols.nvim",
+		"wbthomason/packer.nvim",
+		"ojroques/vim-oscyank",
+		--'williamboman/nvim-lsp-installer'
+		{
+			"williamboman/mason.nvim",
+			config = function()
+				require("mason").setup({
+					ui = {
+						icons = {
+							server_installed = "✓",
+							server_pending = "➜",
+							server_uninstalled = "✗",
+						},
+					},
+				})
+			end,
+		},
+		{
+			"williamboman/mason-lspconfig.nvim",
+			config = function()
+				require("mason-lspconfig").setup({
+					ensure_installed = {
+						"pyright",
+						"gopls",
+						"eslint",
+						"bashls",
+						"html",
+						"lua_ls",
+						"jsonls",
+						"clangd",
+						"lemminx",
+					},
+				})
+			end,
+		},
 
-    --'tpope/vim-commentary',
-    "tpope/vim-fugitive",
+		--'tpope/vim-commentary',
+		"tpope/vim-fugitive",
 
-    "ellisonleao/gruvbox.nvim",
-    "vim-airline/vim-airline",
-    "vim-airline/vim-airline-themes",
+		"ellisonleao/gruvbox.nvim",
+		"vim-airline/vim-airline",
+		"vim-airline/vim-airline-themes",
 
-    "rafcamlet/nvim-luapad",
-    "L3MON4D3/LuaSnip",
-    "saadparwaiz1/cmp_luasnip",
-    {
-      "hrsh7th/nvim-cmp",
-      dependencies = {},
-      config = function()
-        local cmp = require("cmp")
-        local luasnip = require("luasnip")
-        cmp.setup({
-          snippet = {
-            expand = function(args)
-              luasnip.lsp_expand(args.body)
-            end,
-          },
-          window = {
-            -- completion = cmp.config.window.bordered(),
-            -- documentation = cmp.config.window.bordered(),
-          },
-          mapping = cmp.mapping.preset.insert({
-            ["<C-d>"] = cmp.mapping.scroll_docs(-4),
-            ["<C-f>"] = cmp.mapping.scroll_docs(4),
-            ["<C-Space>"] = cmp.mapping.complete(),
-            ["<CR>"] = cmp.mapping.confirm({
-              behavior = cmp.ConfirmBehavior.Replace,
-              select = true,
-            }),
-            ["<Tab>"] = cmp.mapping(function(fallback)
-              if cmp.visible() then
-                cmp.select_next_item()
-              elseif luasnip.expand_or_jumpable() then
-                luasnip.expand_or_jump()
-              else
-                fallback()
-              end
-            end, { "i", "s" }),
-            ["<S-Tab>"] = cmp.mapping(function(fallback)
-              if cmp.visible() then
-                cmp.select_prev_item()
-              elseif luasnip.jumpable(-1) then
-                luasnip.jump(-1)
-              else
-                fallback()
-              end
-            end, { "i", "s" }),
-          }),
-          sources = {
-            { name = "nvim_lsp" },
-            { name = "luasnip" },
-            { name = "buffer" },
-            { name = "nvim_lsp_signature_help" },
-          },
-        })
-      end,
-    },
-    "hrsh7th/cmp-nvim-lsp",
-    "hrsh7th/cmp-buffer",
-    { "numtostr/BufOnly.nvim",                    cmd = "BufOnly" },
+		"rafcamlet/nvim-luapad",
+		"L3MON4D3/LuaSnip",
+		"saadparwaiz1/cmp_luasnip",
+		"hrsh7th/cmp-nvim-lsp",
+		"hrsh7th/cmp-buffer",
+		{ "numtostr/BufOnly.nvim", cmd = "BufOnly" },
 
-    -- Telescope
-    "nvim-telescope/telescope-file-browser.nvim",
-    { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
-    "kyazdani42/nvim-web-devicons",
+		-- Telescope
+		"nvim-telescope/telescope-file-browser.nvim",
+		{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+		"kyazdani42/nvim-web-devicons",
 
-    -- Notes - Wiki
-    "lervag/wiki.vim",
+		-- Notes - Wiki
+		"lervag/wiki.vim",
 
-    -- browse things.
-    "preservim/tagbar",
-    --'preservim/nerdtree',
+		-- browse things.
+		"preservim/tagbar",
+		--'preservim/nerdtree',
 
-    -- Debugging
-    "mfussenegger/nvim-dap-python",
+		-- Debugging
+		"mfussenegger/nvim-dap-python",
 
-    {
-      "kylechui/nvim-surround",
-      version = "*", -- Use for stability; omit to use `main` branch for the latest features
-      event = "VeryLazy",
-      config = function()
-        require("nvim-surround").setup({
-          -- Configuration here, or leave empty to use defaults
-        })
-      end,
-    },
-  },
-  -- mini icons used for oil.nvim
-  { "echasnovski/mini.nvim", version = false },
-  {
-    "vhyrro/luarocks.nvim",
-    priority = 1000, -- Very high priority is required, luarocks.nvim should run as the first plugin in your config.
-    config = true,
-  },
-  {
-    "ojroques/nvim-bufdel",
-    config = function()
-      require("bufdel").setup({
-        next = "tabs",
-        quit = true, -- quit Neovim when last buffer is closed
-      })
-    end,
-  },
+		{
+			"kylechui/nvim-surround",
+			version = "*", -- Use for stability; omit to use `main` branch for the latest features
+			event = "VeryLazy",
+			config = function()
+				require("nvim-surround").setup({
+					-- Configuration here, or leave empty to use defaults
+				})
+			end,
+		},
+	},
+	-- mini icons used for oil.nvim
+	{ "echasnovski/mini.nvim", version = false },
+	{
+		"vhyrro/luarocks.nvim",
+		priority = 1000, -- Very high priority is required, luarocks.nvim should run as the first plugin in your config.
+		config = true,
+	},
+	{
+		"ojroques/nvim-bufdel",
+		config = function()
+			require("bufdel").setup({
+				next = "tabs",
+				quit = true, -- quit Neovim when last buffer is closed
+			})
+		end,
+	},
 }
