58eb14033845df213402ff54ea13b46fd8e79a21
Rodrigo Valdés <rmvaldesd@gmail.com>
2025-05-31 23:22:25 -0400

    copilot is working

---
+++ dotconfig/nvim/lua/plugins/copilot.lua
@@ -0,0 +1,31 @@
+return {
+    "zbirenbaum/copilot.lua",
+    cmd = "Copilot",
+    event = "InsertEnter",
+    config = function()
+        require("copilot").setup({
+            suggestion = {
+                enabled = true,
+                auto_trigger = true
+            },
+            panel = { enabled = false },
+            filetypes = {
+                markdown = true,   -- overrides default
+                terraform = false, -- disallow specific filetype
+                go = true,
+                javascript = true,
+                python = true,
+                typescript = true,
+                lua = true,
+                sh = function()
+                    if string.match(vim.fs.basename(vim.api.nvim_buf_get_name(0)), '^%.env.*') then
+                        -- disable for .env files
+                        return false
+                    end
+                    return true
+                end,
+                ["*"] = false -- disallow for all other filetypes
+            },
+        })
+    end,
+}
