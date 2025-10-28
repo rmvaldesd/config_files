3728c95608e15a11de752662d57c7994aa1a2790
rodrigo.valdes <rodrigo.valdes@uber.com>
2025-10-28 21:02:35 +0000

    updating lualine theme

--- dotconfig/nvim/lua/plugins/lualine.lua
+++ dotconfig/nvim/lua/plugins/lualine.lua
@@ -4,7 +4,7 @@ return {
   config = function()
     require('lualine').setup({
       options = {
-        theme = 'gruvbox',
+        theme = 'tokyonight',
         component_separators = { left = '', right = '' },
         section_separators = { left = '', right = '' },
         globalstatus = true,
