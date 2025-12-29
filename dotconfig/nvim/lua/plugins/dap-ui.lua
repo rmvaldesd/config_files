7442a05c6d46a72b4252b93f6d6665ff3a22a1c1
rodrigo.valdes <rodrigo.valdes@uber.com>
2025-12-29 14:43:21 +0000

    dap ui changes

--- dotconfig/nvim/lua/plugins/dap-ui.lua
+++ dotconfig/nvim/lua/plugins/dap-ui.lua
@@ -1,22 +1,7 @@
 return {
-	"rcarriga/nvim-dap-ui",
-	dependencies = { "mfussenegger/nvim-dap", "nvim-neotest/nvim-nio" },
-	config = function()
-		require("dapui").setup({
-			icons = { expanded = "▾", collapsed = "▸", current_frame = "*" },
-			controls = {
-				icons = {
-					pause = "⏸",
-					play = "▶",
-					step_into = "⏎",
-					step_over = "⏭",
-					step_out = "⏮",
-					step_back = "b",
-					run_last = "▶▶",
-					terminate = "⏹",
-					disconnect = "⏏",
-				},
-			},
-		})
-	end,
+  "rcarriga/nvim-dap-ui",
+  dependencies = { "mfussenegger/nvim-dap", "nvim-neotest/nvim-nio" },
+  config = function()
+    require("dapui").setup()
+  end,
 }
